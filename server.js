9061d3a918fe64f66a8362abd595e48bfec0b06d
Minh Doan <minhadoan@gmail.com>
2023-01-22 14:20:41 -0600

    troubleshooting

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Body Parsing
-app.use(express.json({ limit: '50mb'} ));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Logging
 app.use(logger("dev"));
