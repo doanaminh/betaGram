501d3df98c73bcc4418a20db6955055dab823256
Minh Doan <minhadoan@gmail.com>
2023-01-21 18:21:04 -0600

    troubleshooting

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,6 @@ const path = require("path");
 
 module.exports = multer({
   storage: multer.diskStorage({}),
-  limits: { fileSize: maxSize },
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
     console.log(ext);
